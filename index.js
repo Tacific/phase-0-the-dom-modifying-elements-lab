e3a6e675dc78981ea724bd5bd8ec12d9c2c2e73e
Chantha Voeun <cvoeun45@gmail.com>
2023-04-17 22:46:59 -0400

    DONE

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+const removeMain = document.querySelector('main#main');
+removeMain.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = 'victory'
+
+newHeader.innerHTML = "YOUR-NAME is the champion"
+
+
+
+
+
